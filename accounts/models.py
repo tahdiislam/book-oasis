81cb0ac188853b3b434b138c63ae2f6e6c4cef3a
Tahdi Islam <tshdi60@gmail.com>
2024-06-01 05:58:05 +0600

    add book and book category features

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
+    balance = models.DecimalField(default=0, max_digits=12, decimal_places=2)
+    birth_day = models.DateField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.user.first_name
--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from categories.models import Category
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    categories = models.ManyToManyField(Category)
+    price = models.DecimalField(max_digits=12, decimal_places=0)
+    image = models.ImageField(upload_to='books/media/uploads/', null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return self.title
